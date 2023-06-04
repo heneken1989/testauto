6b4203c23b39eba9a5f0d84c196e2501b4236e98
huyhien <huyhien1989@gmail.com>
2023-06-04 14:15:37 +0900

    update Jenkin file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,19 @@
+pipeline {
+    agent any 
+    stages {
+        stage('Test') {
+            steps {
+                sh ('mvn test')
+            }
+        }
+    }
+
+        stages {
+        stage('Report') {
+            steps {
+                publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'target/karate-reports/', reportFiles: 'karate-timeline.html', reportName: 'HTML Report', reportTitles: '', useWrapperFileDirectly: true])
+            }
+        }
+    }
+    
+}
